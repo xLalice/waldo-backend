ab46bf3d4f5d07b8fa4671341004e0968e48cb9e
JL <lorenzinocentes@gmail.com>
2024-07-11 00:50:41 +0800

    Remove deprecated options

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
